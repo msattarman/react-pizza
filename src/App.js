14abb4974492d52ac513d61e8b55376f1197a2a0
Marsel Sattarov <fsc-3@tages.ru>
2023-05-17 18:01:54 +0500

    render pizzas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json'
+
 import './scss/app.scss';
+
+console.log(pizzas);
 
 function App() {
    return (
@@ -19,8 +23,15 @@ function App() {
                </div>
                <h2 className="content__title">Все пиццы</h2>
                <div className="content__items">
-                  <PizzaBlock title={'чиз'} price={123}/>
-                  <PizzaBlock title={'чивыаываз'} price={5451} />
+                  {
+                     pizzas.map(obj => <PizzaBlock
+                        title={obj.title}
+                        price={obj.price}
+                        imageUrl={obj.imageUrl}
+                        sizes={obj.sizes}
+                        types={obj.types}
+                     />)
+                  }
                </div>
             </div>
          </div>
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 
-function PizzaBlock({title, price}) {
+function PizzaBlock({ title, price, imageUrl, sizes, types }) {
+   const typeNames = ['тонкое', 'традиционное'];
 
    return (
       <div className="pizza-block">
          <img
             className="pizza-block__image"
-            src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+            src={imageUrl}
             alt="Pizza"
          />
          <h4 className="pizza-block__title">{title}</h4>
          <div className="pizza-block__selector">
             <ul>
-               <li className="active">тонкое</li>
-               <li>традиционное</li>
+               {
+                  types.map((type) => <li>{typeNames[type]}</li>)
+               }
             </ul>
             <ul>
-               <li className="active">26 см.</li>
-               <li>30 см.</li>
-               <li>40 см.</li>
+               {
+                  sizes.map((size) => <li>{size} см.</li>)
+               }
             </ul>
          </div>
          <div className="pizza-block__bottom">
@@ -37,7 +39,7 @@ function PizzaBlock({title, price}) {
                   />
                </svg>
                <span>Добавить</span>
-               <i>2</i>
+               <i>0</i>
             </button>
          </div>
       </div>
