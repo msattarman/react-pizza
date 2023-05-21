b95c53e2513b0b5d4bd55efe3cdbe39caea900d1
Marsel Sattarov <fsc-3@tages.ru>
2023-05-21 19:11:43 +0500

    add cart layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,10 +14,11 @@ const Home = () => {
          .then((arr) => {
             setItems(arr);
             setIsLoading(false);
-         })
+         });
+      window.scrollTo(0, 0);
    }, [])
    return (
-      <>
+      <div className="container">
          <div className="content__top">
             <Categories />
             <Sort />
@@ -29,7 +30,7 @@ const Home = () => {
                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
             }
          </div>
-      </>
+      </div>
    )
 }
 
