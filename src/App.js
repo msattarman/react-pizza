3fda1da2dce6da83887f021744147b0cc90420f2
Marsel Sattarov <fsc-3@tages.ru>
2023-05-23 20:18:09 +0500

    context home-search

--- src/App.js
+++ src/App.js
@@ -9,20 +9,24 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './components/NotFoundBlock';
 
+export const SearchContext = React.createContext();
+
 
 function App() {
    const [searchValue, setSearchValue] = React.useState('');
 
    return (
       <div className="wrapper">
-         <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-         <div className="content">
+         <SearchContext.Provider value={{ searchValue, setSearchValue }} >
+            <Header />
+            <div className="content">
                <Routes>
-                  <Route path='/' element={<Home searchValue={searchValue} />} />
-                  <Route path='/cart' element={<Cart/>} />
-                  <Route path='*' element={<NotFound/>} />
+                  <Route path='/' element={<Home />} />
+                  <Route path='/cart' element={<Cart />} />
+                  <Route path='*' element={<NotFound />} />
                </Routes>
-         </div>
+            </div>
+         </SearchContext.Provider>
       </div>
    );
 }
