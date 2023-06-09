2713a7bd6bf37a2a4509b4c11c02bb1e99f4f280
Marsel Sattarov <fsc-3@tages.ru>
2023-06-09 19:13:41 +0500

    typed

---
+++ src/components/NotFoundBlock/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './NotFoundBlock.module.scss'
+
+const NotFoundBlock: React.FC = () => {
+   return (
+      <div className={styles.root} >
+         <h1>
+            <span>😐</span>
+            <br />
+            Not Found
+         </h1>
+         <p>Missing page</p>
+      </div>
+   )
+}
+
+export default NotFoundBlock;
