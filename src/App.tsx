01c9f213d99adc7d81df9c0caf24e4f0de3745cf
Marsel Sattarov <fsc-3@tages.ru>
2023-06-09 19:18:44 +0500

    create MaitLayout

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,15 @@ import './scss/app.scss';
 function App() {
    return (
       <div className="wrapper">
-            <Header />
-            <div className="content">
-               <Routes>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/cart' element={<Cart />} />
-                  <Route path='/pizza/:id' element={<FullPizza />} />
-                  <Route path='*' element={<NotFound />} />
-               </Routes>
-            </div>
+         <Header />
+         <div className="content">
+            <Routes>
+               <Route path='/' element={<Home />} />
+               <Route path='/cart' element={<Cart />} />
+               <Route path='/pizza/:id' element={<FullPizza />} />
+               <Route path='*' element={<NotFound />} />
+            </Routes>
+         </div>
       </div>
    );
 }
