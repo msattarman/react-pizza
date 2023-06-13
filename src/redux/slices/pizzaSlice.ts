e48a89c41940d6ded9530ec558d61463b8c01799
Marsel Sattarov <fsc-3@tages.ru>
2023-06-13 19:50:24 +0500

    pizzaSlice file is typed

--- src/redux/slices/pizzaSlice.ts
+++ src/redux/slices/pizzaSlice.ts
@@ -1,14 +1,9 @@
 import axios from 'axios';
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from '../store';
+import { CartItem } from './cartSlice';
 
-export const fetchPizzas = createAsyncThunk('pizza/fetchPizzasStatus', async (params) => {
-   const { sortBy, order, category, search, currentPage } = params;
-
-   const { data } = await axios.get(`https://64664993ba7110b6639ccaf5.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`);
-
-   return data;
-});
+// type FetchPizzasArgs = Record<string, string>
 
 type Pizza = {
    id: string;
@@ -18,40 +13,75 @@ type Pizza = {
    sizes: number[];
    types: number[];
    rating: number;
+};
+
+export enum Status {
+   LOADING = 'loading',
+   SUCCESS = 'success',
+   ERROR = 'error',
 }
 
 interface PizzaSliceState {
    items: Pizza[];
-   status: 'loading' | 'success' | 'error';
+   status: Status;
 }
 
 const initialState: PizzaSliceState = {
    items: [],
-   status: 'loading', // loading | success | error
+   status: Status.LOADING, // loading | success | error
 };
+
+export const fetchPizzas = createAsyncThunk<Pizza[], Record<string, string>>(
+   'pizza/fetchPizzasStatus',
+   async (params) => {
+      const { sortBy, order, category, search, currentPage } = params;
+
+      const { data } = await axios.get<Pizza[]>(`https://64664993ba7110b6639ccaf5.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`);
+
+      return data;
+   });
 
 const pizzaSlice = createSlice({
    name: 'pizza',
    initialState,
    reducers: {
-      setItems(state, action) {
+      setItems(state, action: PayloadAction<Pizza[]>) {
          state.items = action.payload;
       },
    },
-   extraReducers: {
-      [fetchPizzas.pending]: (state) => {
-         state.status = 'loading';
+   extraReducers: (builder) => {
+      builder.addCase(fetchPizzas.pending, (state, action) => {
+         state.status = Status.LOADING;
          state.items = [];
-      },
-      [fetchPizzas.fulfilled]: (state, action) => {
+      });
+
+      builder.addCase(fetchPizzas.fulfilled, (state, action) => {
          state.items = action.payload;
-         state.status = 'success'
-      },
-      [fetchPizzas.rejected]: (state, action) => {
-         state.status = 'error';
+         state.status = Status.SUCCESS;
+      });
+
+      builder.addCase(fetchPizzas.rejected, (state, action) => {
+         state.status = Status.ERROR;
          state.items = [];
-      },
-   },
+      });
+   }
+
+   // Вариант без typescript и без типизации асинхронных action
+   
+   // extraReducers: {
+   //    [fetchPizzas.pending]: (state) => {
+   //       state.status = 'loading';
+   //       state.items = [];
+   //    },
+   //    [fetchPizzas.fulfilled]: (state, action) => {
+   //       state.items = action.payload;
+   //       state.status = 'success'
+   //    },
+   //    [fetchPizzas.rejected]: (state, action) => {
+   //       state.status = 'error';
+   //       state.items = [];
+   //    },
+   // },
 });
 
 export const selectPizzaData = (state: RootState) => state.pizza;
