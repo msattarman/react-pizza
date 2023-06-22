e34002ae06aac773b55da277788630e1ba576c77
Marsel Sattarov <fsc-3@tages.ru>
2023-06-22 23:34:20 +0500

    add lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
+import React, { Suspense } from 'react';
 import { Route, Routes } from "react-router-dom";
 
 import Header from './components/Header';
 
 import Home from './pages/Home';
-import Cart from './pages/Cart';
-import FullPizza from './pages/FullPizza';
-import NotFound from './components/NotFoundBlock';
+// import Cart from './pages/Cart';
+// import FullPizza from './pages/FullPizza';
+// import NotFound from './components/NotFoundBlock';
+
 
 import './scss/app.scss';
+import MainLayout from "./layouts/MainLayout";
+
+const Cart = React.lazy(() => import(/* webpackChunkName: 'Cart' */ './pages/Cart'))
+const FullPizza = React.lazy(() => import(/* webpackChunkName: 'FullPizza' */ './pages/FullPizza'))
+const NotFound = React.lazy(() => import(/* webpackChunkName: 'NotFound' */ './pages/NotFound'))
 
 function App() {
    return (
-      <div className="wrapper">
-         <Header />
-         <div className="content">
-            <Routes>
-               <Route path='/' element={<Home />} />
-               <Route path='/cart' element={<Cart />} />
-               <Route path='/pizza/:id' element={<FullPizza />} />
-               <Route path='*' element={<NotFound />} />
-            </Routes>
-         </div>
-      </div>
+      // <div className="wrapper">
+      //    <Header />
+      //    <div className="content">
+      <Routes >
+         <Route path='/' element={<MainLayout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart'
+               element={<Suspense fallback={<div>Загрузка корзины...</div>}>
+                  <Cart />
+               </Suspense>}
+            />
+            <Route path='/pizza/:id'
+               element={<Suspense fallback={<div>Загрузка описания...</div>}>
+                  <FullPizza />
+               </Suspense>} />
+            <Route path='*'
+               element={<Suspense fallback={<div>Загрузка страницы...</div>}>
+                  <NotFound />
+               </Suspense>} />
+         </Route>
+      </Routes>
+      //    </div>
+      // </div>
    );
 }
 
--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
@@ -36,6 +37,11 @@ const FullPizza: React.FC = () => {
          <img src={pizza.imageUrl} />
          <h2>{pizza.title}</h2>
          <h4>{pizza.price} P</h4>
+         <Link to='/'>
+            <button className="button button--outline button--add">
+               <span>Назад</span>
+            </button>
+         </Link>
       </div>
    )
 }
